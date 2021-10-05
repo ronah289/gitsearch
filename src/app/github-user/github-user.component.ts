a0e2ecf7409457c0827560237f284414369509f4
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-05 10:24:50 -0800

    create,define and test find-user function

---
+++ src/app/github-user/github-user.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { GitserviceService } from '../services/gitservice.service';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-github-user',
+  templateUrl: './github-user.component.html',
+  styleUrls: ['./github-user.component.css']
+})
+export class GithubUserComponent implements OnInit {
+  user!: User;
+
+  constructor(private gitService: GitserviceService) {
+
+  }
+  findThisUser(nameOfUser: any) {
+    this.gitService.findUser(nameOfUser).then((success) => {
+      this.user = this.gitService.user
+    })
+  }
+
+  ngOnInit(): void {
+    this.findThisUser('ronah289')
+  }
+
+}
---
+++ src/app/services/gitservice.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+
+import { User } from '../user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GitserviceService {
+  user!: User;
+
+  constructor(private http:HttpClient) { 
+    this.user = new User('','','','',0,0,0,)
+  }
+  findUser(nameOfUser:any){
+    interface GithubApi{
+      login:string,
+      html_url:string,
+      repos_url:string,
+      name:string,
+      public_repos:number,
+      followers:number,
+      following:number,
+    }
+    let headers = new HttpHeaders({'Authorization':'token ' + environment.pass})
+    let searchFor =environment.baseUrl + nameOfUser ;
+    let inHead={headers:headers}
+    let promise = new Promise((resolve, reject) => {
+      this.http.get<GithubApi>(searchFor,inHead).toPromise().then((response) => { this.user = response;
+            
+            resolve(resolve);
+            console.log(this.user)
+          },
+          (error) => {
+            reject();
+            console.log(error)
+          }
+        );
+    });
+    return promise;
+  }
+}
