d44707657166bd96dee53fdc4932f48414c49fe4
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-05 18:21:20 -0800

    add token in header

--- src/app/github-user/github-user.component.ts
+++ src/app/github-user/github-user.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { GitserviceService } from '../services/gitservice.service';
 import { User } from '../user';
+import { Repository } from '../repository';
 
 @Component({
   selector: 'app-github-user',
@@ -10,15 +11,24 @@ import { User } from '../user';
   styleUrls: ['./github-user.component.css']
 })
 export class GithubUserComponent implements OnInit {
-  user!: User;
+  user?: User;
+  repository!: Repository | any;
 
   constructor(private gitService: GitserviceService) {
+    this.repository = new Repository('', '', '', '',)
 
   }
   findThisUser(whatToSearch: any) {
     this.gitService.findUser(whatToSearch).then((success) => {
       this.user = this.gitService.user
     })
+    this.gitService.showRepositories(whatToSearch).then((success) => {
+      this.repository = this.gitService.repository;
+    },
+      (error) => {
+        console.log(error)
+      }
+    )
   }
 
   ngOnInit(): void {
--- src/app/services/gitservice.service.ts
+++ src/app/services/gitservice.service.ts
@@ -30,7 +30,7 @@ export class GitserviceService {
       bio:string,
       avatar_url:string,
     }
-    let headers = new HttpHeaders({'Authorization':'token ' + environment.pass})
+    let headers = new HttpHeaders({'Authorization':'token' + environment.pass})
     let searchFor = environment.baseUrl + whatToSearch ;
     let inHead={headers:headers}
     let promise = new Promise((resolve, reject) => {
@@ -50,16 +50,18 @@ export class GitserviceService {
   //end of user processor
 
   // handle repository search
-  showRepositories(whatToSearch:any){
+  showRepositories(user:any){
     interface GithubApi{
       html_url:string,
       description:string,
       language:string,
       homepage:string
     }
-    let url = environment.pass  + whatToSearch + '/repos' + '?access_token=' + environment.pass;
+    let headers = new HttpHeaders({'Authorization':'token' + environment.pass})
+    let inHead={headers:headers}
+    let url = environment.baseUrl  + user + '/repos';
     let promise = new Promise((resolve, reject) => {
-      this.http.get<GithubApi>(url).toPromise().then(response => {
+      this.http.get<GithubApi>(url,inHead).toPromise().then(response => {
         this.repository = response;
         resolve(resolve)
   
